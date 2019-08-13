8b4f54c0ae45a3725977ab515b15ad9623be8486
Ralph Maas <rmaas@compete.nl>
2019-08-13 20:21:34 +0200

    reengineered

--- ESP8266-WeatherHTTPClient-NRF24.ino
+++ ESP8266-WeatherHTTPClient-NRF24.ino
@@ -1,3 +1,4 @@
+
 #include <FS.h>
 #include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
 #include <ArduinoJson.h>          //https://github.com/bblanchon/ArduinoJson
@@ -6,16 +7,28 @@
 //needed for library
 #include <ESP8266WebServer.h>
 #include <DNSServer.h>
-#include <WiFiManager.h>          //https://github.com/kentaylor/WiFiManager
 #include "rmCommon.h"
+#include "rmKNMIAPISetup.h"
+
+#include <SPI.h> //Call SPI library so you can communicate with the nRF24L01+
+//#include <nRF24L01.h> //nRF2401 libarary found at https://github.com/tmrh20/RF24/
+//#include <RF24.h> //nRF2401 libarary found at https://github.com/tmrh20/RF24/
+#include <WiFiManager.h>          //https://github.com/kentaylor/WiFiManager
+
+//NRF24
+/*
+const uint8_t PIN_NRF24_CE = 7; //This pin is used to set the nRF24 to standby (0) or active mode (1)
+const uint8_t PIN_NRF24_CSN = 8; //This pin is used to tell the nRF24 whether the SPI communication is a command
+RF24 wirelessSPI(PIN_NRF24_CE, PIN_NRF24_CSN); // Declare object from nRF24 library (Create your wireless SPI) 
+const uint64_t wAddress = 0xB00B1E50C3LL;  //Create pipe address to send data, the "LL" is for LongLong type
+const uint8_t rFChan = 89; //Set channel default (chan 84 is 2.484GHz to 2.489GHz)
+const uint8_t rDelay = 7; //this is based on 250us increments, 0 is 250us so 7 is 2 ms
+const uint8_t rNum = 5; //number of retries that will be attempted 
+*/
 
 
-// Constants
-const int PIN_LED = 2; // D4 on NodeMCU and WeMos. Controls the onboard LED.
-const int PIN_LED_CONFIG = 4; // D2 In config mode.
-const int TRIGGER_PIN = 0; // D3 on NodeMCU and WeMos, set MCU con configmode.
+
 const char* CONFIG_FILE = "/config.json";
-const int API_ON = 14; //D5 set the api read on. Off is only for test.
 bool READ_API = false;
 
 // Variables
@@ -40,118 +53,67 @@ char knmiApiKeyCity[20] = "";
 bool readConfigFile();
 bool writeConfigFile();
 
-// Setup function
-void setup() {
-  // Put your setup code here, to run once
-  Serial.begin(115200);
-  Serial.println("\n Starting");
-
-
-  // Initialize the LED digital pin as an output.
-  pinMode(PIN_LED, OUTPUT);
-  pinMode(PIN_LED_CONFIG, OUTPUT);
-  // Initialize trigger pins
-  pinMode(TRIGGER_PIN, INPUT_PULLUP);
-  pinMode(API_ON, INPUT_PULLUP);
-  
-  
-  // Mount the filesystem
-  bool result = SPIFFS.begin();
-  Serial.println("SPIFFS opened: " + result);
-
-  if (!readConfigFile()) {
-    Serial.println("Failed to read configuration file, using default values");
-  }
-
-  WiFi.printDiag(Serial); //Remove this line if you do not want to see WiFi password printed
-
-  if (WiFi.SSID() == "") {
-    Serial.println("We haven't got any access point credentials, so get them now");
-    initialConfig = true;
-  } else {
-    digitalWrite(PIN_LED, HIGH); // Turn LED on as we are  in configuration mode.
-    digitalWrite(PIN_LED_CONFIG, LOW); // Turn LED off as we are in configuration mode.
-    WiFi.mode(WIFI_STA); // Force to station mode because if device was switched off while in access point mode it will start up next time in access point mode.
-    unsigned long startedAt = millis();
-    Serial.print("After waiting ");
-    int connRes = WiFi.waitForConnectResult();
-    float waited = (millis()- startedAt);
-    Serial.print(waited/1000);
-    Serial.print(" secs in setup() connection result is ");
-    Serial.println(connRes);
-  }
-
-  if (WiFi.status()!=WL_CONNECTED){
-    Serial.println("Failed to connect, finishing setup anyway");
-  } else{
-    Serial.print("Local ip: ");
-    Serial.println(WiFi.localIP());
-  }
-  
-}
-
-// Loop function
-
-void loop() {
-  checkConfigurationRequested();
-  if (WiFi.status() == WL_CONNECTED) 
-  {
-    unsigned long currentMillis = millis();
-    if (currentMillis - previousMillis >= interval)
-    {
-      READ_API = digitalRead(API_ON) == LOW;
-      previousMillis = currentMillis;
-      HTTPClient http; //Object of class HTTPClient
+void checkConfigurationRequested(){
+  // is configuration portal requested?
+  if ((digitalRead(PIN_SWITCH_CONFIG) == LOW) || (initialConfig)) {
+     Serial.println("Configuration portal requested");
  
-      ReadKNMIApi(http);
+     digitalWrite(PIN_LED_GREEN, LOW); // turn the LED on by making the voltage LOW to tell us we are in configuration mode.
+     digitalWrite(PIN_LED_CONFIG_BLUE, HIGH); // In configmode.
+    
+    //Local intialization. Once its business is done, there is no need to keep it around
 
-      Serial.println("Base Data");
-      Serial.println(weather_base_info.PackId);
-      Serial.println(weather_base_info.City);
-      Serial.println(weather_base_info.Temp);
-      Serial.println(weather_base_info.AirPress);
-      Serial.println(weather_base_info.Sup);
-      Serial.println(weather_base_info.Sunder);
+    // Extra parameters to be configured
+    // After connecting, parameter.getValue() will get you the configured value
+    // Format: <ID> <Placeholder text> <default value> <length> <custom HTML> <label placement>
+    // KNMI API Key - this is a straight forward string parameter
+    WiFiManagerParameter p_knmiApiKey("knmiApiKey", "KNMI API Key", knmiApiKey, 17);
+    WiFiManagerParameter p_knmiApiKeyCity("knmiApiKeyCity", "City", knmiApiKeyCity, 20);
+    
+    // Just a quick hint
+    WiFiManagerParameter p_hint("<small>*Hint: if you want to reuse the currently active WiFi credentials, leave SSID and Password fields empty</small>");
+    
+    // Initialize WiFIManager
+    WiFiManager wifiManager;
+    
+    //add all parameters here
+    
+    wifiManager.addParameter(&p_hint);
+    wifiManager.addParameter(&p_knmiApiKey);
+    wifiManager.addParameter(&p_knmiApiKeyCity);
+   
 
-      if (READ_API )
-      {
-
-        Serial.println(weather_today.PackId);
-        Serial.println(weather_today.Icon);
-        Serial.println(weather_today.MaxTemp);
-        Serial.println(weather_today.MinTemp);
-        Serial.println(weather_today.Wind);
-        Serial.println(weather_today.WindDir);
-        Serial.println(weather_today.RainProcent);
-        Serial.println(weather_today.SunProcent);
-
-        Serial.println(weather_tomorrow.PackId);
-        Serial.println(weather_tomorrow.Icon);
-        Serial.println(weather_tomorrow.MaxTemp);
-        Serial.println(weather_tomorrow.MinTemp);
-        Serial.println(weather_tomorrow.Wind);
-        Serial.println(weather_tomorrow.WindDir);
-        Serial.println(weather_tomorrow.RainProcent);
-        Serial.println(weather_tomorrow.SunProcent);
-
-        Serial.println(weather_DayAfterTomorrow.PackId);
-        Serial.println(weather_DayAfterTomorrow.Icon);
-        Serial.println(weather_DayAfterTomorrow.MaxTemp);
-        Serial.println(weather_DayAfterTomorrow.MinTemp);
-        Serial.println(weather_DayAfterTomorrow.Wind);
-        Serial.println(weather_DayAfterTomorrow.WindDir);
-        Serial.println(weather_DayAfterTomorrow.RainProcent);
-        Serial.println(weather_DayAfterTomorrow.SunProcent);
- 
-        Serial.println(weather_text.PackId);
-        Serial.println(weather_text.Text);
-      }
+    // It starts an access point 
+    // and goes into a blocking loop awaiting configuration.
+    // Once the user leaves the portal with the exit button
+    // processing will continue
+    if (!wifiManager.startConfigPortal()) {
+      Serial.println("Not connected to WiFi but continuing anyway.");
+    } else {
+      // If you get here you have connected to the WiFi
+      Serial.println("Connected...yeey :)");
     }
+
+
+    // Getting posted form values and overriding local variables parameters
+    // Config file is written regardless the connection state
+    strcpy(knmiApiKey, p_knmiApiKey.getValue());
+    strcpy(knmiApiKeyCity, p_knmiApiKeyCity.getValue());
+
+    
+    // Writing JSON config file to flash for next boot
+    writeConfigFile();
+
+    
+   // digitalWrite(PIN_LED_GREEN, HIGH); // Turn LED off as we are not in configuration mode.
+
+    ESP.reset(); // This is a bit crude. For some unknown reason webserver can only be started once per boot up 
+    // so resetting the device allows to go back into config mode again when it reboots.
+    delay(5000);
   }
 }
 
-void ReadKNMIApi(HTTPClient& http)
-{
+void ReadKNMIApi(HTTPClient& http){
     readConfigFile();
     Serial.print("API Key: ");
     Serial.println(knmiApiKey);
@@ -272,67 +234,6 @@ void ReadKNMIApi(HTTPClient& http)
       }
 }
 
-void checkConfigurationRequested()
-{
-  // is configuration portal requested?
-  if ((digitalRead(TRIGGER_PIN) == LOW) || (initialConfig)) {
-     Serial.println("Configuration portal requested");
- 
-     digitalWrite(PIN_LED, LOW); // turn the LED on by making the voltage LOW to tell us we are in configuration mode.
-     digitalWrite(PIN_LED_CONFIG, HIGH); // In cofigmode.
-    
-    //Local intialization. Once its business is done, there is no need to keep it around
-
-    // Extra parameters to be configured
-    // After connecting, parameter.getValue() will get you the configured value
-    // Format: <ID> <Placeholder text> <default value> <length> <custom HTML> <label placement>
-    // KNMI API Key - this is a straight forward string parameter
-    WiFiManagerParameter p_knmiApiKey("knmiApiKey", "KNMI API Key", knmiApiKey, 17);
-    WiFiManagerParameter p_knmiApiKeyCity("knmiApiKeyCity", "City", knmiApiKeyCity, 20);
-    
-    // Just a quick hint
-    WiFiManagerParameter p_hint("<small>*Hint: if you want to reuse the currently active WiFi credentials, leave SSID and Password fields empty</small>");
-    
-    // Initialize WiFIManager
-    WiFiManager wifiManager;
-    
-    //add all parameters here
-    
-    wifiManager.addParameter(&p_hint);
-    wifiManager.addParameter(&p_knmiApiKey);
-    wifiManager.addParameter(&p_knmiApiKeyCity);
-   
-
-    // It starts an access point 
-    // and goes into a blocking loop awaiting configuration.
-    // Once the user leaves the portal with the exit button
-    // processing will continue
-    if (!wifiManager.startConfigPortal()) {
-      Serial.println("Not connected to WiFi but continuing anyway.");
-    } else {
-      // If you get here you have connected to the WiFi
-      Serial.println("Connected...yeey :)");
-    }
-
-
-    // Getting posted form values and overriding local variables parameters
-    // Config file is written regardless the connection state
-    strcpy(knmiApiKey, p_knmiApiKey.getValue());
-    strcpy(knmiApiKeyCity, p_knmiApiKeyCity.getValue());
-
-    
-    // Writing JSON config file to flash for next boot
-    writeConfigFile();
-
-    
-    digitalWrite(PIN_LED, HIGH); // Turn LED off as we are not in configuration mode.
-
-    ESP.reset(); // This is a bit crude. For some unknown reason webserver can only be started once per boot up 
-    // so resetting the device allows to go back into config mode again when it reboots.
-    delay(5000);
-  }
-}
-
 bool readConfigFile() {
   // this opens the config file in read-mode
   File f = SPIFFS.open(CONFIG_FILE, "r");
@@ -401,3 +302,133 @@ bool writeConfigFile() {
   return true;
 }
   
+// Setup function
+void setup() {
+  // Put your setup code here, to run once
+  Serial.begin(115200);
+  Serial.println("\n Starting");
+  SetupLedAndSwitches();
+ 
+  //NRF24
+  /*
+  wirelessSPI.begin();  //Start the nRF24 module
+  wirelessSPI.setChannel(rFChan); 
+  wirelessSPI.setRetries(rDelay,rNum); //if a transmit fails to reach receiver (no ack packet) then this sets retry attempts and delay between retries   
+  wirelessSPI.openWritingPipe(wAddress); //open writing or transmit pipe
+  wirelessSPI.stopListening(); //go into transmit mode
+  */
+  
+  // Mount the filesystem
+  bool result = SPIFFS.begin();
+  Serial.println("SPIFFS opened: " + result);
+
+  if (!readConfigFile()) {
+    Serial.println("Failed to read configuration file, using default values");
+  }
+
+  WiFi.printDiag(Serial); //Remove this line if you do not want to see WiFi password printed
+
+  if (WiFi.SSID() == "") {
+    Serial.println("We haven't got any access point credentials, so get them now");
+    initialConfig = true;
+  } else {
+    digitalWrite(PIN_LED_GREEN, HIGH); // Turn LED on as we are  in configuration mode.
+    digitalWrite(PIN_LED_CONFIG_BLUE, LOW); // Turn LED off as we are in configuration mode.
+    WiFi.mode(WIFI_STA); // Force to station mode because if device was switched off while in access point mode it will start up next time in access point mode.
+    unsigned long startedAt = millis();
+    Serial.print("After waiting ");
+    int connRes = WiFi.waitForConnectResult();
+    float waited = (millis()- startedAt);
+    Serial.print(waited/1000);
+    Serial.print(" secs in setup() connection result is ");
+    Serial.println(connRes);
+  }
+
+  if (WiFi.status()!=WL_CONNECTED){
+    Serial.println("Failed to connect, finishing setup anyway");
+  } else{
+    Serial.print("Local ip: ");
+    Serial.println(WiFi.localIP());
+  }
+  
+}
+
+// Loop function
+
+void loop() {
+  checkConfigurationRequested();
+  if (WiFi.status() == WL_CONNECTED) 
+  {
+    unsigned long currentMillis = millis();
+    if (currentMillis - previousMillis >= interval)
+    {
+     // READ_API = digitalRead(PIN_SWITCH_API_ON) == LOW;
+      previousMillis = currentMillis;
+      HTTPClient http; //Object of class HTTPClient
+ 
+      ReadKNMIApi(http);
+
+      Serial.println("Base Data");
+      Serial.println(weather_base_info.PackId);
+      Serial.println(weather_base_info.City);
+      Serial.println(weather_base_info.Temp);
+      Serial.println(weather_base_info.AirPress);
+      Serial.println(weather_base_info.Sup);
+      Serial.println(weather_base_info.Sunder);
+
+  /*    if (READ_API )
+      {
+
+          Serial.println(weather_today.PackId);
+          Serial.println(weather_today.Icon);
+          Serial.println(weather_today.MaxTemp);
+          Serial.println(weather_today.MinTemp);
+          Serial.println(weather_today.Wind);
+          Serial.println(weather_today.WindDir);
+          Serial.println(weather_today.RainProcent);
+          Serial.println(weather_today.SunProcent);
+  
+          Serial.println(weather_tomorrow.PackId);
+          Serial.println(weather_tomorrow.Icon);
+          Serial.println(weather_tomorrow.MaxTemp);
+          Serial.println(weather_tomorrow.MinTemp);
+          Serial.println(weather_tomorrow.Wind);
+          Serial.println(weather_tomorrow.WindDir);
+          Serial.println(weather_tomorrow.RainProcent);
+          Serial.println(weather_tomorrow.SunProcent);
+  
+          Serial.println(weather_DayAfterTomorrow.PackId);
+          Serial.println(weather_DayAfterTomorrow.Icon);
+          Serial.println(weather_DayAfterTomorrow.MaxTemp);
+          Serial.println(weather_DayAfterTomorrow.MinTemp);
+          Serial.println(weather_DayAfterTomorrow.Wind);
+          Serial.println(weather_DayAfterTomorrow.WindDir);
+          Serial.println(weather_DayAfterTomorrow.RainProcent);
+          Serial.println(weather_DayAfterTomorrow.SunProcent);
+   
+          Serial.println(weather_text.PackId);
+          Serial.println(weather_text.Text);
+        }
+        */
+/*
+        Serial.println("start NRF24");
+       //Send default info
+       if (!wirelessSPI.write(&weather_base_info, sizeof(weather_base_info))){  //send data and remember it will retry if it fails
+          delay(random(5,20)); //as another back up, delay for a random amount of time and try again
+          if (!wirelessSPI.write(&weather_base_info, sizeof(weather_base_info))){
+            //set error flag if it fails again
+             Serial.println("NRF24 - Error sending message");
+
+          }
+       }
+        Serial.println("NRF24 send message");
+       
+     //Send default today data
+     //Send default tomorrow data
+     //Send default day after today data
+     //Send default day text data
+    }
+ */
+    }
+  }
+}
